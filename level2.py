a227fbccfb15e80d59b8bf7729a7048edee6da98
izz13 <94653804+izz13@users.noreply.github.com>
2022-09-12 16:47:20 -0700

    added level1 and level2 file

---
+++ level2.py
@@ -0,0 +1,33 @@
+import pygame
+
+def level2loop():
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    fps = 60
+
+    def dungeonScene():
+        screen.fill(white)
+
+    scene = "dungeonScene"
+    isRunning = True
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                isRunning = False
+
+        if scene == "dungeonScene":
+            dungeonScene()
+
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    level2loop()
