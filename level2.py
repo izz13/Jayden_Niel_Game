d730561137f3daaf2f2f59472796634894509873
neiln <neilnabar5@gmail.com>
2022-09-25 10:25:39 -0700

    Added dungeon scenes 1, 2, 3, & 4

--- level2.py
+++ level2.py
@@ -1,4 +1,5 @@
 import pygame
+from objects import player
 
 def level2loop():
     screen_width = 800
@@ -11,21 +12,30 @@ def level2loop():
     DungeonImg1 = pygame.image.load("DungeonScene1.png")
     DungeonImg2 = pygame.image.load("DungeonScene2.png")
     DungeonImg3 = pygame.image.load("DungeonScene3.png")
+    DungeonImg4 = pygame.image.load("DungeonScene4.png")
 
 
     def dungeonScene1():
+        screen.fill(white)
         screen.blit(DungeonImg1, (0, 0))
+        player.render(screen)
 
     def dungeonScene2():
+        screen.fill(white)
         screen.blit(DungeonImg2, (0, 0))
+        player.render(screen)
 
     def dungeonScene3():
+        screen.fill(white)
         screen.blit(DungeonImg3, (0, 0))
+        player.render(screen)
 
     def dungeonScene4():
-        pass
+        screen.fill(white)
+        screen.blit(DungeonImg4, (0, 0))
+        player.render(screen)
 
-    scene = "dungeonScene"
+    scene = "dungeonScene1"
     isRunning = True
     while isRunning:
         for event in pygame.event.get():
@@ -33,14 +43,43 @@ def level2loop():
                 pygame.quit()
                 isRunning = False
 
-        if scene == "dungeonScene":
-            dungeonScene()
+    scene = "dungeonScene2"
+    isRunning = True
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                isRunning = False
 
+    scene = "dungeonScene3"
+    isRunning = True
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                isRunning = False
 
-        pygame.display.flip()
-        clock.tick(fps)
+    scene = "dungeonScene4"
+    isRunning = True
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                isRunning = False
 
+        if scene == "dungeonScene1":
+            dungeonScene1()
+        elif scene == "dungeonScene2":
+            dungeonScene2()
+        elif scene == "dungeonScene3":
+            dungeonScene3()
+        elif scene == "dungeonScene4":
+            dungeonScene4()
+
+    pygame.display.flip()
+    clock.tick(fps)
     pygame.quit()
+
 
 if __name__ == "__main__":
     level2loop()
