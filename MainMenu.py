02dc73f162ff67c1e0779115a40e3001b5fd9779
jaydenchoi <jaydenchoi2013@gmail.com>
2022-11-21 16:39:58 -0800

    added main menu buttons

--- MainMenu.py
+++ MainMenu.py
@@ -11,21 +11,34 @@ def mainMenuloop():
     fps = 60
     menubkgd = pygame.image.load("main_menu_pngs/menubackground.png")
     playbttn = pygame.image.load("main_menu_pngs/play_button.png")
+    exitbttn = pygame.image.load("main_menu_pngs/exit_button.png")
+    settingbttn = pygame.image.load("main_menu_pngs/settings.png")
+    creditbttn = pygame.image.load("main_menu_pngs/credits.png")
     playbttn_rect = playbttn.get_bounding_rect()
+    exitbttn_rect = exitbttn.get_bounding_rect()
+    settingbttn_rect = settingbttn.get_bounding_rect()
+    creditbttn_rect = creditbttn.get_bounding_rect()
     playbttn_rect.center = [180,250]
+    exitbttn_rect.center = [650,450]
+    settingbttn_rect.center = [650, 250]
+    creditbttn_rect.center = [180, 450]
 
 
     isRunning = True
     while isRunning:
         mouse_pos = pygame.mouse.get_pos()
-        print(mouse_pos)
+        #print(mouse_pos)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 isRunning = False
 
-        screen.blit(menubkgd,[0,0])
-        screen.blit(playbttn,[80,200])
+        screen.blit(menubkgd, [0, 0])
+        screen.blit(playbttn, [80, 200])
+        screen.blit(exitbttn, [500, 400])
+        screen.blit(settingbttn, [500, 200])
+        screen.blit(creditbttn, [80, 400])
+
         #pygame.draw.rect(screen,white,playbttn_rect)
         pygame.display.flip()
         clock.tick(fps)
