b7cb7cbdf19db25916ae05f80cf45e46ed3a3d9c
izz13 <94653804+izz13@users.noreply.github.com>
2023-02-11 10:52:22 -0800

    fixed level4

--- level4.py
+++ level4.py
@@ -1,6 +1,12 @@
 import pygame
+import time
+import objects
+from objects import player
+from objects import Platform
+import enemy
 
 pygame.init()
+
 
 def level4loop():
     screen_width = 800
@@ -12,6 +18,37 @@ def level4loop():
     green = (0, 255, 0)
     clock = pygame.time.Clock()
     fps = 60
-    font = pygame.font.SysFont(None, 60)
+    #font = pygame.font.SysFont(None, 60)
+
+    player.pos = [0,0]
+    scene = "scene1"
+
+    def scene1(events,time):
+        screen.fill(white)
+
+    isRunning = True
+    while isRunning:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                isRunning = False
+
+        time = clock.get_time() / fps
+        if player.pos[0] < 0:
+            player.pos[0] = 0
+        if player.pos[0] > 745:
+            player.pos[0] = 745
+        if scene == "scene1":
+            scene1(events,time)
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+pygame.quit()
+
+if __name__ == "__main__":
+    level4loop()
 pygame.quit()
 quit()
