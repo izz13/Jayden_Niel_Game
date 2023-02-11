7110d1610034d259261ec5bb38276916e24e7cfe
jaydenchoi <jaydenchoi2013@gmail.com>
2023-02-11 10:40:56 -0800

    added lvl 4 sorta

---
+++ level4.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+def level4loop():
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    gray = (178, 190, 181)
+    green = (0, 255, 0)
+    clock = pygame.time.Clock()
+    fps = 60
+    font = pygame.font.SysFont(None, 60)
+pygame.quit()
+quit()
